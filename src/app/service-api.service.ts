b10e04347ccca6f2fe6f7a35830cca89166e9967
samsepi0l <samsepi0l@localhost.localdomain>
2020-04-14 15:19:58 -0300

    Alteração na URL da API consultando por nome da cidade

--- src/app/service-api.service.ts
+++ src/app/service-api.service.ts
@@ -7,11 +7,11 @@ import { zip } from 'rxjs';
 })
 export class ServiceApiService {
 
-  weatherApi: string = "https://api.weatherbit.io/v2.0/current?postal_code=";
+  weatherApi: string = "https://api.weatherbit.io/v2.0/current?city=";
 
   constructor(private http: HttpClient) { }
 
   getApiDate(zipcode) {
-    return this.http.get(this.weatherApi + zipcode + "&country=BR&lang=pt&city=S%C3%A3o%20Paulo&key=4c6b53bd14e84267b6e70da5b3e6d08d");
+    return this.http.get(this.weatherApi + zipcode + "&country=BR&lang=pt&key=4c6b53bd14e84267b6e70da5b3e6d08d");
   }
 }
