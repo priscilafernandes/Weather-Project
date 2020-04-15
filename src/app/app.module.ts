7aa5c42ff1c59334a43547a6e3590aaafa9bf7d4
erianerik <erianerik@gmail.com>
2020-04-15 13:56:56 -0300

    Impleementação do grafico ao component display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,30 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 // Import Form Control
 import { FormsModule } from "@angular/forms";
+// Import Charts Module
+
+import { ChartsModule } from 'ng2-charts';
+
 
 import { AppComponent } from './app.component';
 import { DisplayWeatherRealComponent } from './display-weather-real/display-weather-real.component';
 import { SearchWeatherComponent } from './search-weather/search-weather.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GrapichTodayComponent } from './grapich-today/grapich-today.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DisplayWeatherRealComponent,
     SearchWeatherComponent,
+    GrapichTodayComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
